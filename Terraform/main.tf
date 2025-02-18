d08311e91eb7c6d88bb7c0a222c60c79a5755d79
SohelBiswas <88239407+SohelBiswas@users.noreply.github.com>
2025-02-18 11:16:19 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+resource "aws_vpc" "sohel-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_eks_cluster" "sohel-cluster" {
+  name     = "sohel-cluster"
+  role_arn = aws_iam_role.eks.arn
+}
